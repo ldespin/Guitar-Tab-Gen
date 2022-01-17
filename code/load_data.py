68358ec58b2baf8a03c3f4a9cca720006a63e0d7
ldespin <lucas.despin@outlook.fr>
2022-01-17 13:59:24 +0100

    test guitar only

--- code/load_data.py
+++ code/load_data.py
@@ -66,6 +66,8 @@ def load_test(data_path="../test_data",guitar_only="on"):
     song_dir = os.listdir(data_path)
     input_texts = []
     target_texts = []
+    input_texts_guitar = []
+    target_texts_guitar = []
     headers = []
 
     for song in song_dir:
@@ -73,14 +75,25 @@ def load_test(data_path="../test_data",guitar_only="on"):
         header = open("{}/{}/{}.header.txt".format(data_path,song,song))
         for i in range(1,len(measures)-1):
             #gathering of input texts
+            
             input_file = open("{}/{}/{}.measure_{}.txt".format(data_path,song,song,i))
             input_text = list(input_file)
             input_texts.append(input_text)
+            input_text_guitar = []
+            for token in target_text:
+                if guitar_only=="on" and "bass" not in token and "drums" not in token:
+                    input_text_guitar.append(token)
 
             #gathering of target texts
             target_file = open("{}/{}/{}.measure_{}.txt".format(data_path,song,song,i+1))
             target_text = list(target_file)
             target_texts.append(target_text)
+
+            target_text_guitar = []
+            for token in target_text:
+                if guitar_only=="on" and "bass" not in token and "drums" not in token:
+                    target_text_guitar.append(token)
+
             headers.append(header)
 
 
