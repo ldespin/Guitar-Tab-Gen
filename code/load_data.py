35aa29d6e2ca2490ef58614e58dead30211fb75d
ldespin <lucas.despin@outlook.fr>
2022-02-06 16:37:57 +0100

    fix

--- code/load_data.py
+++ code/load_data.py
@@ -1,33 +1,30 @@
 import os
 import numpy as np
-from sklearn.model_selection import train_test_split
+import keras
+import vectorize_data
 
 def get_tokens(data_path="../training_data/raw",guitar_only="on"):
-    song_dir = os.listdir(data_path)
+    measures = os.listdir(data_path)
 
     target_tokens = []
     max_tokens = 0
 
-    for song in song_dir:
-        measures = os.listdir("{}/{}".format(data_path,song))
-        for i in range(1,len(measures)):
-            #gathering of input texts
-            input_file = open("{}/{}/{}.measure_{}.txt".format(data_path,song,song,i))
-            input_text = list(input_file)
-            input_text_guitar = []
-        
-            for token in input_text:
-                    if guitar_only=="on" and "bass" not in token and "drums" not in token and "nfx" not in token:
-                        input_text_guitar.append(token)
-                        if token not in target_tokens:
-                            target_tokens.append(token)
-
-                    elif guitar_only=="off":
-                        if token not in target_tokens:
-                            target_tokens.append(token)
-
-            if len(input_text_guitar)>max_tokens:
-                max_tokens = len(input_text_guitar)
+    for measure in measures:
+        #gathering of input texts
+        input_file = open(measure)
+        input_text = list(input_file)
+        input_text_guitar = []
+    
+        for token in input_text:
+                if guitar_only=="on" and "bass" not in token and "drums" not in token and "nfx" not in token:
+                    input_text_guitar.append(token)
+                    if token not in target_tokens:
+                        target_tokens.append(token)
+                elif guitar_only=="off":
+                    if token not in target_tokens:
+                        target_tokens.append(token)
+        if len(input_text_guitar)>max_tokens:
+            max_tokens = len(input_text_guitar)
 
     target_tokens = sorted(target_tokens)
     
@@ -41,58 +38,61 @@ def get_tokens(data_path="../training_data/raw",guitar_only="on"):
     return target_tokens, max_tokens
 
 
-def load_training(data_path="../training_data",guitar_only="on"):
+class DataGenerator(keras.utils.Sequence):
+    'Generates data for Keras'
+    def __init__(self, list_IDs, labels, dim, batch_size=32, n_channels=1,
+                 n_classes=10, shuffle=True):
+        'Initialization'
+        self.dim = dim
+        self.batch_size = batch_size
+        self.labels = labels
+        self.list_IDs = list_IDs
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.shuffle = shuffle
+        self.on_epoch_end()
 
-    song_dir = os.listdir(data_path)
+    def __len__(self):
+        'Denotes the number of batches per epoch'
+        return int(np.floor(len(self.list_IDs) / self.batch_size))
 
-    input_texts = []
-    target_texts = []
-    input_texts_guitar = []
-    target_texts_guitar = []
-    target_tokens = []
+    def __getitem__(self, index):
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
 
-    for song in song_dir:
-        measures = os.listdir("{}/{}".format(data_path,song))
-        for i in range(1,len(measures)-1):
-            #gathering of input texts
-            input_file = open("{}/{}/{}.measure_{}.txt".format(data_path,song,song,i))
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
+
+    def on_epoch_end(self):
+        'Updates indexes after each epoch'
+        self.indexes = np.arange(len(self.list_IDs))
+        if self.shuffle == True:
+            np.random.shuffle(self.indexes)
+
+    def __data_generation(self, list_IDs_temp):
+        'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
+        # Initialization
+        X = np.empty((self.batch_size, *self.dim, self.n_channels))
+        y = np.empty((self.batch_size, *self.dim, self.n_channels))
+
+        # Generate data
+        for i, ID in enumerate(list_IDs_temp[:-1]):
+            # Store sample
+            input_file = open('..training_data/raw/' + ID + '.txt')
             input_text = list(input_file)
-            input_text_guitar = []
-            input_texts.append(input_text)
 
-            #gathering of target texts
-            target_file = open("{}/{}/{}.measure_{}.txt".format(data_path,song,song,i+1))
+            target_file = open('..training_data/raw/' + list_IDs_temp[i+1] + '.txt')
             target_text = list(target_file)
-            target_text_guitar = []
-            target_texts.append(target_text)
-            
-            for token in input_text:
-                if guitar_only=="on" and "bass" not in token and "drums" not in token and "nfx" not in token:
-                    if token not in target_tokens:
-                        target_tokens.append(token)
-                    input_text_guitar.append(token)
 
-                elif guitar_only=="off":
-                    if token not in target_tokens:
-                        target_tokens.append(token)
-            
-            for token in target_text:
-                if guitar_only=="on" and "bass" not in token and "drums" not in token and "nfx" not in token:
-                    if token not in target_tokens:
-                        target_tokens.append(token)
-                    target_text_guitar.append(token)
+            encoder_input_data, decoder_input_data, decoder_target_data = vectorize_data.vectorize_training(input_text, target_text)
 
-                elif guitar_only=="off":
-                    if token not in target_tokens:
-                        target_tokens.append(token)
-            
-            input_texts_guitar.append(input_text_guitar)
-            target_texts_guitar.append(target_text_guitar)
-
-
-    if guitar_only=="on":
-        return input_texts_guitar, target_texts_guitar, target_tokens
-    return input_texts, target_texts, target_tokens
+        return encoder_input_data, decoder_input_data, decoder_target_data
 
 def load_test(data_path="../test_data",guitar_only="on"):
 
